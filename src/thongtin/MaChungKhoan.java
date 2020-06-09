a27997a4a29da7a590ff996c9bb28746c1ffcabe
Anh030300 <62802177+Anh030300@users.noreply.github.com>
2020-06-10 00:17:49 +0700

    Update MaChungKhoan.java

--- src/thongtin/MaChungKhoan.java
+++ src/thongtin/MaChungKhoan.java
@@ -1,18 +1,6 @@
 package thongtin;
 
 import java.time.LocalDate;
-
-public class MaChungKhoan {
-    private String tenMa, tenCongTi;
-    LocalDate thoiGian;
-    Gia gia;
-    GiaoDich giaoDich;
-
-    public MaChungKhoan(String tenMa, String tenCongTi, Gia gia, GiaoDich giaoDich, LocalDate thoiGian){
-        this.tenMa = tenMa;
-        this.gia = gia;package thongtin;
-
-import java.time.LocalDate;
 import java.util.Date;
 
 public class MaChungKhoan {
@@ -43,35 +31,6 @@ public class MaChungKhoan {
     }
 
     public Date getThoiGian() {
-        return thoiGian;
-    }
-
-    public Gia getGia() {
-        return gia;
-    }
-
-    public GiaoDich getGiaoDich() {
-        return giaoDich;
-    }
-
-    public void setTenCongTi(String tenCongTi) {
-        this.tenCongTi = tenCongTi;
-    }
-}
-
-        this.giaoDich = giaoDich;
-        this.thoiGian = thoiGian;
-    }
-
-    public String getTenMa() {
-        return tenMa;
-    }
-
-    public String getTenCongTi() {
-        return tenCongTi;
-    }
-
-    public LocalDate getThoiGian() {
         return thoiGian;
     }
 
