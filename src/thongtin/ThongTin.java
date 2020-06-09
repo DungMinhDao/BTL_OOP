28ef6c9845b4be1bbfc29a308cad364345736653
hiep1307 <64054312+hiep1307@users.noreply.github.com>
2020-06-09 23:03:14 +0700

    Add files via upload

---
+++ src/thongtin/ThongTin.java
@@ -0,0 +1,8 @@
+package thongtin;
+
+import java.util.HashMap;
+
+public class ThongTin {
+    public static HashMap<String, MaChungKhoan> maChungKhoanHashMap;
+    public static HashMap<String, NhomNganh> nhomNganhHashMap;
+}
