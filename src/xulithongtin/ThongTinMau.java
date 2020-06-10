6cbd0adfc95532bd396527d326ee55a15dbe53ce
hiep1307 <64054312+hiep1307@users.noreply.github.com>
2020-06-10 21:51:45 +0700

    Update ThongTinMau.java

--- src/xulithongtin/ThongTinMau.java
+++ src/xulithongtin/ThongTinMau.java
@@ -4,9 +4,7 @@ import maucau.Mau1;
 import maucau.MauCau;
 
 public abstract class ThongTinMau {
-    public MauCau khoiTaoMau(){
-        return null;
-    };
+    public abstract MauCau khoiTaoMau();
     public void inCau(){
         System.out.println(khoiTaoMau().layCau());
     }
