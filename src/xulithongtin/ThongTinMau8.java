2084e3789c7f0da256b66265ac0ba9d7fae0b60b
dnkhanh45 <66416653+dnkhanh45@users.noreply.github.com>
2020-06-10 23:03:48 +0700

    Add files via upload

--- src/xulithongtin/ThongTinMau8.java
+++ src/xulithongtin/ThongTinMau8.java
@@ -1,27 +1,35 @@
 package xulithongtin;
 
+import java.util.ArrayList;
 import maucau.Mau8;
 import maucau.MauCau;
-import thongtin.NhomNganh;
+import thongtin.MaChungKhoan;
 import thongtin.ThongTin;
 
-import java.util.ArrayList;
+public class ThongTinMau8 extends ThongTinMau{
 
-public class ThongTinMau8 extends ThongTinMau {
+    private static final int MAX = 5;
 
     @Override
     public MauCau khoiTaoMau() {
-        ArrayList<NhomNganh> nhomNganhTang, nhomNganhGiam;
-        nhomNganhTang = new ArrayList<NhomNganh>();
-        nhomNganhGiam = new ArrayList<NhomNganh>();
-        for(NhomNganh nhomNganh: ThongTin.nhomNganhHashMap.values()){
-            if(nhomNganh.getTongThayDoi() > 0){
-                nhomNganhTang.add(nhomNganh);
+        ArrayList<String> top5Tang = new ArrayList<String>();
+        ArrayList<String> top5Giam = new ArrayList<String>();
+        float min = 0, max = 0;
+        String tenMaTangTop = null, tenMaGiamTop = null;
+        for (int i = 0; i < MAX; i++) {
+            for (String tenMa : maChungKhoanHashMap.keys()) {
+                if((!top5Tang.contains(tenMa)) && (max < maChungKhoanHashMap.get(tenMa).getGia().getTyLeThayDoi())){
+                    max = maChungKhoanHashMap.get(tenMa).getGia().getTyLeThayDoi();
+                    tenMaTangTop = tenMa;
+                }
+                if((!top5Giam.contains(tenMa)) && (min < maChungKhoanHashMap.get(tenMa).getGia().getTyLeThayDoi())) {
+                    min = maChungKhoanHashMap.get(tenMa).getGia().getTyLeThayDoi();
+                    tenMaGiamTop = tenMa;
+                }
             }
-            else if(nhomNganh.getTongThayDoi() < 0){
-                nhomNganhGiam.add(nhomNganh);
-            }
+            top5Tang.add(tenMaTangTop);
+            top5Giam.add(tenMaGiamTop);
         }
-        return new Mau8(nhomNganhGiam, nhomNganhTang);
+        return new Mau8(top5Tang, top5Giam);
     }
 }
