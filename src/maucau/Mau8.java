b0a26dcb55e61b7a9ef179470d120599ab6ef6f8
hiep1307 <64054312+hiep1307@users.noreply.github.com>
2020-06-10 23:08:49 +0700

    Update Mau8.java

--- src/maucau/Mau8.java
+++ src/maucau/Mau8.java
@@ -1,129 +1,103 @@
 package maucau;
-
-import java.util.ArrayList;
+import java.util.*;
+import thongtin.MaChungKhoan;
+import thongtin.NhomNganh;
 
 public class Mau8 extends MauCau{
+    private static String tag = "Tổng quan về các nhóm ngành";
+    private ArrayList<NhomNganh> nhomNganhGiam, nhomNganhTang;
 
-    private static String tag = "Top 5 tăng giảm";
-    ArrayList<String> top5Tang = new ArrayList<>();
-    ArrayList<String> top5Giam = new ArrayList<>();
-    private static final int MAX = 5;
+    public Mau8(ArrayList<NhomNganh> nhomNganhGiam, ArrayList<NhomNganh> nhomNganhTang){
+        this.nhomNganhGiam = nhomNganhGiam;
+        this.nhomNganhTang = nhomNganhTang;
 
-    public Mau8(ArrayList<String> top5Tang, ArrayList<String> top5Giam){
-        this.top5Tang = top5Tang;
-        this.top5Giam = top5Giam;
+        Collections.sort(this.nhomNganhTang, new Comparator<NhomNganh>() {
+            @Override
+            public int compare(NhomNganh n1, NhomNganh n2){
+                if(n1.getTongThayDoi() < n2.getTongThayDoi()) return 1;
+                else if(n1.getTongThayDoi() > n2.getTongThayDoi()) return -1;
+                else return 0;
+            }
+        });
+
+        Collections.sort(this.nhomNganhGiam, new Comparator<NhomNganh>() {
+            @Override
+            public int compare(NhomNganh n1, NhomNganh n2){
+                if(n1.getTongThayDoi() < n2.getTongThayDoi()) return -1;
+                else if(n1.getTongThayDoi() > n2.getTongThayDoi()) return 1;
+                else return 0;
+            }
+        });
     }
 
-    // Câu tăng top 1 loại 1
-    public void cauTangTop1_Loai1(){
-        MaChungKhoan ma = maChungKhoanHashMap.get(top5Tang[0]);
-        tapCau.add("Đứng ở vị trí đầu tiên là cổ phiếu " + ma.getTenMa() + " của " + ma.getTenCongTy() + " với mức tăng " + ma.getThayDoiTyLe() + "%.");
+    public void thuThapCau(){
+        cau1();
+        cau2();
+        cau3();
+        cau4();
+        cau5();
     }
 
-    // Câu tăng top 1 loại 2
-    public void cauTangTop1_Loai2(){
-        MaChungKhoan ma = maChungKhoanHashMap.get(top5Tang[0]);
-        tapCau.add("Đứng đầu bảng xếp hạng là " + ma.getTenMa() + " của " + ma.getTenCongTy() + " với mức tăng trưởng " ma.getThayDoiTyLe() + "%.");
-    }
-
-    // Câu tăng thứ tự bất kỳ 1-5 loại 1
-    public void cauTang_Loai1(){
-        for (int i = 0; i < MAX; i++) {
-            MaChungKhoan ma = maChungKhoanHashMap.get(top5Tang[i]);
-            tapCau.add("Đứng thứ " + (i + 1) + " là cổ phiếu " + ma.getTenMa() + "của " + ma.getTenCongTy() + "với mức tăng hơn " + ma.getThayDoiTyLe() + " %.");
+    public void cau1(){
+        String s = "";
+        if(nhomNganhGiam.size() >= nhomNganhTang.size()*3){
+            s = "Hầu hết các nhóm ngành trên thị trường đều giảm điểm.";
+        }
+        else if(nhomNganhTang.size() >= nhomNganhGiam.size()*3){
+            s = "Hầu hết các nhóm ngành trên thị trường đều tăng điểm.";
+        }
+        if(!s.isEmpty()) {
+            tapCau.add(s);
         }
     }
 
-    // Câu tăng thứ tự bất kỳ 1-5 loại 2
-    public void cauTang_Loai2(){
-        for (int i = 0; i < MAX; i++) {
-            MaChungKhoan ma = maChungKhoanHashMap.get(top5Tang[i]);
-            tapCau.add("Trong đó, " + ma.getTenMa()+ " của" + ma.getTenCongTy() + " đứng ở vị trí thứ " + (i + 1) + " với mức tăng " ma.getThayDoiTyLe() + "%.");
+    public void cau2(){
+        String s = "Nhóm ngành giảm mạnh nhất là nhóm ngành ";
+        s = s + nhomNganhGiam.get(0).getTenNhomNganh() + " với mức giảm " + (- nhomNganhGiam.get(0).getTongThayDoi()) + " điểm.";
+
+        tapCau.add(s);
+    }
+
+    public void cau3(){
+        String s = "Nhóm ngành tăng mạnh nhất là nhóm ngành ";
+        s = s + nhomNganhTang.get(0).getTenNhomNganh() + " với mức tăng " + nhomNganhTang.get(0).getTongThayDoi() + " điểm.";
+
+        tapCau.add(s);
+    }
+
+    public void cau4(){
+        int i;
+        String s = "Các cổ phiếu có tính thị trường hơn như " + nhomNganhTang.get(1).getTenNhomNganh();
+        int length = Math.min(nhomNganhTang.size(), 6);
+        for(i = 2; i < length; i++){
+            s = s + ", " + nhomNganhTang.get(i).getTenNhomNganh();
         }
+        s = s + ",... cũng thu hút dòng tiền khá mạnh.";
+        tapCau.add(s);
     }
 
-    // Câu tăng thứ tự bất kỳ 1-5 loại 3
-    public void cauTang_Loai3(){
-        for (int i = 0; i < MAX; i++) {
-            MaChungKhoan ma = maChungKhoanHashMap.get(top5Tang[i]);
-            tapCau.add("Vị trí thứ " + (i + 1) + " là cổ phiếu " + ma.getTenMa() + " của " + ma.getTenCongTy() + "với mức tăng trưởng đạt gần " + ma.getThayDoiTyLe() + "%.");
+    public void cau5(){
+        if(nhomNganhTang.size() > nhomNganhGiam.size()){
+            String s = "Đà tăng cũng lan tỏa sang một số ngành khác như ";
+            for(int i = 0; i <= nhomNganhTang.size(); ++i){
+                if(i == 4){
+                    break;
+                }
+                s += nhomNganhTang.get(i).getTenNhomNganh() + ", ";
+            }
+            s += "...";
+            tapCau.add(s);
         }
-    }
-
-    // Câu tăng thứ tự bất kỳ 1-5 loại 4
-    public void cauTang_Loai4(){
-        for (int i = 0; i < MAX; i++) {
-            MaChungKhoan ma = maChungKhoanHashMap.get(top5Tang[i]);
-            tapCau.add("Vị trí thứ " + (i + 1) + " thuộc về cổ phiếu " + ma.getTenMa() + " của " + ma.getTenCongTy() + " với mức tăng trưởng " ma.getThayDoiTyLe() + "%.");
+        if(nhomNganhGiam.size() > nhomNganhTang.size()){
+            String s = "Đà suy giảm cũng lan sang một số ngành khác như ";
+            for(int i = 0; i <= nhomNganhGiam.size(); ++i){
+                if(i == 4){
+                    break;
+                }
+                s += nhomNganhGiam.get(i).getTenNhomNganh() + ", ";
+            }
+            s += "...";
+            tapCau.add(s);
         }
-    }
-
-    // Câu tăng thứ tự bất kỳ 1-5 loại 5
-    public void cauTang_Loai5(){
-        for (int i = 0; i < MAX; i++) {
-            MaChungKhoan ma = maChungKhoanHashMap.get(top5Tang[i]);
-            tapCau.add("Đứng thứ " + (i + 1) + " là " + ma.getTenMa() + " của " + ma.getTenCongTy() + " với mức tăng trưởng " + ma.getThayDoiTyLe() + "%.");
-        }
-    }
-
-    // Câu giảm top 1 loại 1
-    public void cauGiamTop1_Loai1(){
-        MaChungKhoan ma = maChungKhoanHashMap.get(top5Giam[0]);
-        tapCau.add("Đứng ở vị trí cuối cùng là cổ phiếu " + ma.getTenMa() + " của " + ma.getTenCongTy() + " với mức giảm " + ma.getThayDoiTyLe() + "%.");
-
-    }
-
-    // Câu giảm top 1 loại 2
-    public void cauGiamTop1_Loai2(){
-        MaChungKhoan ma = maChungKhoanHashMap.get(top5Giam[0]);
-        tapCau.add("Đứng cuối bảng xếp hạng là " + ma.getTenMa() + " của " + ma.getTenCongTy() + " với mức tăng trưởng " ma.getThayDoiTyLe() + "%.");
-    }
-
-    // Câu giảm thứ tự bất kỳ 1-5 loại 1
-    public void cauGiam_Loai1(){
-        for ( int i = 0; i < MAX ; i++) {
-            MaChungKhoan ma = maChungKhoanHashMap.get(top5Giam[i]);
-            tapCau.add("Đứng thứ " + (i + 1) + " là cổ phiếu " + ma.getTenMa() + "của " + ma.getTenCongTy() + "với mức tăng hơn " ma.getThayDoiTyLe() + " %.");
-        }
-    }
-
-    // Câu giảm thứ tự bất kỳ 1-5 loại 2
-    public void cauGiam_Loai2(){
-        for (int i = 0; i < MAX; i++) {
-            MaChungKhoan ma = maChungKhoanHashMap.get(top5Giam[i]);
-            tapCau.add("Trong đó, " + ma.getTenMa() + " của" + ma.getTenCongTy() + " đứng ở vị trí thứ " + (i + 1) + " với mức tăng " + ma.getThayDoiTyLe() + "%.");
-        }
-    }
-
-    // Câu giảm thứ tự bất kỳ 1-5 loại 3
-    public void cauGiam_Loai3(){
-        for (int i = 0; i < MAX; i++) {
-            MaChungKhoan ma = maChungKhoanHashMap.get(top5Giam[i]);
-            tapCau.add("Vị trí thứ " + (i + 1) + " là cổ phiếu " + ma.getTenMa() + " của " + ma.getTenCongTy() + "với mức tăng trưởng đạt gần " + ma.getThayDoiTyLe() + "%.");
-        }
-    }
-
-    // Câu giảm thứ tự bất kỳ 1-5 loại 4
-    public void cauGiam_Loai4(){
-        for (int i = 0; i < MAX; i++) {
-            MaChungKhoan ma = maChungKhoanHashMap.get(top5Giam[i]);
-            tapCau.add("Vị trí thứ " + (i + 1) + " thuộc về cổ phiếu " + ma.getTenMa() + " của " + ma.getTenCongTy() + " với mức tăng trưởng " + ma.getThayDoiTyLe() + "%.");
-        }
-    }
-
-    // Câu giảm thứ tự bất kỳ 1-5 loại 5
-    public void cauTang_Loai5(){
-        for (int i = 0; i < MAX; i++) {
-            MaChungKhoan ma = maChungKhoanHashMap.get(top5Giam[i]);
-            tapCau.add("Đứng thứ " + (i + 1) + " là " + ma.getTenMa() + " của " + ma.getTenCongTy() + " với mức tăng trưởng " + ma.getThayDoiTyLe() + "%.");
-        }
-    }
-
-    public ArrayList<String> getTop5Tang(){
-        return this.top5Tang;
-    }
-
-    public ArrayList<String> getTop5Giam(){
-        return this.top5Giam;
     }
 }
