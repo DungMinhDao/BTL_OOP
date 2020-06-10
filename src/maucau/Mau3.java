6fcbe36300bfeed0d4a2d79d4ceebc065c2ac622
hau1862 <62495260+hau1862@users.noreply.github.com>
2020-06-10 21:38:46 +0700

    Update Mau3.java

--- src/maucau/Mau3.java
+++ src/maucau/Mau3.java
@@ -6,15 +6,63 @@ import java.util.ArrayList;
 
 public class Mau3 extends MauCau {
     private static String tag = "Nhóm ngân hàng";
-    private ArrayList<MaChungKhoan> nhomNganHangTang, nhomNganHangGiam, nhomNganHangDungGia;
+    private ArrayList<MaChungKhoan> nhomNganHangTang, nhomNganHangGiam;
 
     public Mau3(ArrayList<MaChungKhoan> nhomNganHangTang, ArrayList<MaChungKhoan> nhomNganHangGiam){
         this.nhomNganHangTang = nhomNganHangTang;
         this.nhomNganHangGiam = nhomNganHangGiam;
     }
 
+    @Override
+    public void thuThapCau(){
+        cau1();
+        cau2();
+        cau3();
+    }
+
     public void cau1(){
-        String s = "Nhóm ngân hàng suy yếu với";
+        String s = "Nhóm ngân hàng suy yếu với ";
+        int i, length = nhomNganHangGiam.size();
+        float minGiam = nhomNganHangGiam.get(0).getGia().getThayDoiDiem();
+        float maxGiam = minGiam;
+        s = s + nhomNganHangGiam.get(0).getTenMa();
+        for(i = 1; i < length; i++){
+            if(minGiam > nhomNganHangGiam.get(i).getGia().getThayDoiDiem()){
+                minGiam = nhomNganHangGiam.get(i).getGia().getThayDoiDiem()
+            }
+            if(maxGiam < nhomNganHangGiam.get(i).getGia().getThayDoiDiem()){
+                maxGiam = nhomNganHangGiam.get(i).getGia().getThayDoiDiem()
+            }
+            s = s + ", " + nhomNganHangGiam.get(i).getTenMa();
+        }
+
+        s = s + "trượt giá " + minGiam + "-" + maxGiam + " đồng.";
+        tapCau.add(s);
+    }
+
+    public void cau2(){
+        String s = "Nhóm ngân hàng có các mã ";
+        int i, length = nhomNganHangTang.size();
+        float minTang = nhomNganHangTang.get(0).getGia().getThayDoiDiem();
+        float maxTang = minTang;
+        s = s + nhomNganHangTang.get(0).getTenMa();
+        for(i = 1; i < length; i++){
+            if(minTang > nhomNganHangTang.get(i).getGia().getThayDoiDiem()){
+                minTang = nhomNganHangTang.get(i).getGia().getThayDoiDiem()
+            }
+            if(maxTang < nhomNganHangTang.get(i).getGia().getThayDoiDiem()){
+                maxTang = nhomNganHangTang.get(i).getGia().getThayDoiDiem()
+            }
+            s = s + ", " + nhomNganHangTang.get(i).getTenMa();
+        }
+        s = s + " xanh giá, tăng" + minTang + "-" + maxTang + "đồng.";
+        tapCau.add(s);
+    }
+
+    public void cau3(){
+        String s = "";
+
+        tapCau.add(s);
     }
 
     public String getTag() {
