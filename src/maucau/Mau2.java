56022d1a9854d4967a5216c42e9567f32e8c06ad
hau1862 <62495260+hau1862@users.noreply.github.com>
2020-06-11 18:03:23 +0700

    Update Mau2.java

--- src/maucau/Mau2.java
+++ src/maucau/Mau2.java
@@ -6,25 +6,102 @@ import java.util.ArrayList;
 
 public class Mau2 extends MauCau {
     private static String tag = "Một số mã";
-    ArrayList<MaChungKhoan> maChungKhoans;
+    ArrayList<MaChungKhoan> maChungKhoanTang, maChungKhoanGiam, maChungKhoanThamChieu;
 
-    public Mau2(ArrayList<MaChungKhoan> maChungKhoans){
-        this.maChungKhoans = maChungKhoans;
+    public Mau2(final ArrayList<MaChungKhoan> maChungKhoanTang, final ArrayList<MaChungKhoan> maChungKhoanGiam,
+            final ArrayList<MaChungKhoan> maChungKhoanThamChieu) {
+        this.maChungKhoanTang = maChungKhoanTang;
+        this.maChungKhoanGiam = maChungKhoanGiam;
+        this.maChungKhoanThamChieu = maChungKhoanThamChieu;
+    }
+
+    public void thuThapCau() {
+        cau1();
+        cau2();
+        cau3();
     }
 
     public void cau1(){
-        
+        int i;
+        String s = new String();
+        String s1 = new String();
+		float minTang = maChungKhoanTang.get(0).getGia().getThayDoiDiem();
+        float maxTang = minTang;
+        s = s + maChungKhoanTang.get(0).getTenMa();
+        final int length = maChungKhoanTang.size();
+        for(i = 1; i < length; i++){
+            if(maChungKhoanTang.get(i).getGia().getThayDoiDiem() > maxTang){
+                maxTang = maChungKhoanTang.get(i).getGia().getThayDoiDiem();
+            }
+            if(maChungKhoanTang.get(i).getGia().getThayDoiDiem() < minTang){
+                minTang = maChungKhoanTang.get(i).getGia().getThayDoiDiem();
+            }
+
+            if(maChungKhoanTang.get(i).getGia().getDongCua() == maChungKhoanTang.get(i).getGia().getTran()){
+                s1 = s1 + ", " + maChungKhoanTang.get(i).getTenMa();
+            }
+            else{
+                s = s + ", " + maChungKhoanTang.get(i).getTenMa();
+            }
+        }
+
+        s = s + "lần lượt tăng " + minTang + "-" + maxTang + "đồng một cổ phiếu" + s1 + " thi tăng trần.";
+        tapCau.add(s);
     }
 
-    public String getTag() {
+    public void cau2(){
+        String s = new String("Các mã ");
+        int i;
+        s = s + maChungKhoanThamChieu.get(0).getTenMa();
+        for(i = 1; i < maChungKhoanThamChieu.size(); i++){
+            s = s + ", " + maChungKhoanThamChieu.get(i).getTenMa(); 
+        }
+
+        s = s + "đứng mức tham chiếu.";
+        tapCau.add(s);
+    }
+
+    public void cau3(){
+        int i;
+        String s = new String();
+		float minTang = maChungKhoanTang.get(0).getGia().getThayDoiDiem();
+        float maxTang = minTang;
+        s = s + maChungKhoanTang.get(0).getTenMa();
+        int length = maChungKhoanTang.size();
+        for(i = 1; i < length; i++){
+            if(maChungKhoanTang.get(i).getGia().getThayDoiDiem() > maxTang){
+                maxTang = maChungKhoanTang.get(i).getGia().getThayDoiDiem();
+            }
+            if(maChungKhoanTang.get(i).getGia().getThayDoiDiem() < minTang){
+                minTang = maChungKhoanTang.get(i).getGia().getThayDoiDiem();
+            }
+
+            s = s + ", " + maChungKhoanTang.get(i).getTenMa();
+        }
+
+        s = s +  "lần lượt tăng " + minTang + "-" + maxTang + "đồng một cổ phiếu trong khi ";
+        length = maChungKhoanThamChieu.size();
+        for(i = 0; i < length; i++){
+            s = s + ", " + maChungKhoanThamChieu.get(i).getTenMa();
+        }
+
+        s = s + " đi ngang.";
+        tapCau.add(s);
+    }
+
+    public static String getTag() {
         return tag;
     }
 
-    public ArrayList<MaChungKhoan> getMaChungKhoans() {
-        return maChungKhoans;
+    public ArrayList<MaChungKhoan> getMaChungKhoanTang() {
+        return maChungKhoanTang;
     }
 
-    public ArrayList<String> getTapCau(){
-        return tapCau;
+    public ArrayList<MaChungKhoan> getMaChungKhoanGiam() {
+        return maChungKhoanGiam;
+    }
+
+    public ArrayList<MaChungKhoan> getMaChungKhoanThamChieu() {
+        return maChungKhoanThamChieu;
     }
 }
