d759bacc7eb6130b699726de5c1fb8c542d2a0d0
hau1862 <62495260+hau1862@users.noreply.github.com>
2020-06-11 18:19:33 +0700

    Update ThongTinMau8.java

--- src/xulithongtin/ThongTinMau8.java
+++ src/xulithongtin/ThongTinMau8.java
@@ -1,35 +1,22 @@
 package xulithongtin;
-
+import thongtin.*;
 import java.util.ArrayList;
 import maucau.Mau8;
 import maucau.MauCau;
-import thongtin.MaChungKhoan;
-import thongtin.ThongTin;
 
 public class ThongTinMau8 extends ThongTinMau{
-
-    private static final int MAX = 5;
-
     @Override
-    public MauCau khoiTaoMau() {
-        ArrayList<String> top5Tang = new ArrayList<String>();
-        ArrayList<String> top5Giam = new ArrayList<String>();
-        float min = 0, max = 0;
-        String tenMaTangTop = null, tenMaGiamTop = null;
-        for (int i = 0; i < MAX; i++) {
-            for (String tenMa : maChungKhoanHashMap.keys()) {
-                if((!top5Tang.contains(tenMa)) && (max < maChungKhoanHashMap.get(tenMa).getGia().getTyLeThayDoi())){
-                    max = maChungKhoanHashMap.get(tenMa).getGia().getTyLeThayDoi();
-                    tenMaTangTop = tenMa;
-                }
-                if((!top5Giam.contains(tenMa)) && (min < maChungKhoanHashMap.get(tenMa).getGia().getTyLeThayDoi())) {
-                    min = maChungKhoanHashMap.get(tenMa).getGia().getTyLeThayDoi();
-                    tenMaGiamTop = tenMa;
-                }
+    public MauCau khoiTaoMau(){
+        ArrayList<NhomNganh> nhomNganhGiam = new ArrayList<NhomNganh>();
+        ArrayList<NhomNganh> nhomNganhTang = new ArrayList<NhomNganh>();
+        for (final NhomNganh nhom : ThongTin.nhomNganhHashMap.values()) {
+            if(nhom.getTongThayDoi() > 0){
+                nhomNganhTang.add(nhom);
             }
-            top5Tang.add(tenMaTangTop);
-            top5Giam.add(tenMaGiamTop);
+            else{
+                nhomNganhGiam.add(nhom);
+            }
         }
-        return new Mau8(top5Tang, top5Giam);
-    }
+        return new Mau8(nhomNganhGiam, nhomNganhTang);
+    }    
 }
