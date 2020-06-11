e09933c35fe404b0733ecda959ced31b6db79753
hau1862 <62495260+hau1862@users.noreply.github.com>
2020-06-11 18:04:20 +0700

    Update Mau3.java

--- src/maucau/Mau3.java
+++ src/maucau/Mau3.java
@@ -13,8 +13,56 @@ public class Mau3 extends MauCau {
         this.nhomNganHangGiam = nhomNganHangGiam;
     }
 
+    @Override
+    public void thuThapCau(){
+        cau1();
+        cau2();
+        cau3();
+    }
+
     public void cau1(){
-        String s = "Nhóm ngân hàng suy yếu với"
+        String s = new String("Nhóm ngân hàng suy yếu với ");
+        int i, length = nhomNganHangGiam.size();
+        float minGiam = nhomNganHangGiam.get(0).getGia().getThayDoiDiem();
+        float maxGiam = minGiam;
+        s = s + nhomNganHangGiam.get(0).getTenMa();
+        for(i = 1; i < length; i++){
+            if(minGiam > nhomNganHangGiam.get(i).getGia().getThayDoiDiem()){
+                minGiam = nhomNganHangGiam.get(i).getGia().getThayDoiDiem();
+            }
+            if(maxGiam < nhomNganHangGiam.get(i).getGia().getThayDoiDiem()){
+                maxGiam = nhomNganHangGiam.get(i).getGia().getThayDoiDiem();
+            }
+            s = s + ", " + nhomNganHangGiam.get(i).getTenMa();
+        }
+
+        s = s + "trượt giá " + minGiam + "-" + maxGiam + " đồng.";
+        tapCau.add(s);
+    }
+
+    public void cau2(){
+        String s = new String("Nhóm ngân hàng có các mã ");
+        int i, length = nhomNganHangTang.size();
+        float minTang = nhomNganHangTang.get(0).getGia().getThayDoiDiem();
+        float maxTang = minTang;
+        s = s + nhomNganHangTang.get(0).getTenMa();
+        for(i = 1; i < length; i++){
+            if(minTang > nhomNganHangTang.get(i).getGia().getThayDoiDiem()){
+                minTang = nhomNganHangTang.get(i).getGia().getThayDoiDiem();
+            }
+            if(maxTang < nhomNganHangTang.get(i).getGia().getThayDoiDiem()){
+                maxTang = nhomNganHangTang.get(i).getGia().getThayDoiDiem();
+            }
+            s = s + ", " + nhomNganHangTang.get(i).getTenMa();
+        }
+        s = s + " xanh giá, tăng" + minTang + "-" + maxTang + "đồng.";
+        tapCau.add(s);
+    }
+
+    public void cau3(){
+        String s = new String();
+
+        tapCau.add(s);
     }
 
     public String getTag() {
