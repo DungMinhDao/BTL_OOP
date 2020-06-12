1001fa0be0d019bf7b598e65751c4cb2146886a6
hau1862 <62495260+hau1862@users.noreply.github.com>
2020-06-12 10:40:05 +0700

    Update ThongTinMau4.java

--- src/xulithongtin/ThongTinMau4.java
+++ src/xulithongtin/ThongTinMau4.java
@@ -1,5 +1,29 @@
 package xulithongtin;
+import maucau.Mau4;
+import maucau.MauCau;
+import thongtin.*;
+import java.util.ArrayList;
 
-public class ThongTinMau4 {
-
+public class ThongTinMau4 extends ThongTinMau{
+    @Override
+    public MauCau khoiTaoMau(){
+        ArrayList<MaChungKhoan> nhomBlueChipsTang = new ArrayList<MaChungKhoan>();
+        ArrayList<MaChungKhoan> nhomBlueChipsGiam = new ArrayList<MaChungKhoan>();
+        ArrayList<MaChungKhoan> nhomBlueChipsDungGia = new ArrayList<MaChungKhoan>();
+        for(int i = 0; i < NhomVN30.tenMA.size(); ++i){
+            String tenMa = NhomVN30.tenMA.get(i);
+            MaChungKhoan ma = ThongTin.maChungKhoanHashMap.get(tenMa);
+            TinhChatMa tinhChatMa = new TinhChatMa(ma);
+            if(tinhChatMa.tangGia()){
+                nhomBlueChipsTang.add(ma);
+            }
+            else if(tinhChatMa.giamGia()){
+                nhomBlueChipsGiam.add(ma);
+            }
+            else if(tinhChatMa.dungGiaThamChieu()){
+                nhomBlueChipsDungGia.add(ma);
+            }
+        }
+        return new Mau4(nhomBlueChipsGiam, nhomBlueChipsTang, nhomBlueChipsDungGia);
+    }
 }
